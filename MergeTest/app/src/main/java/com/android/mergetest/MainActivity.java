75148249d865e5d38c1283e4de8bc5b085e5e9ae
차종한 <jonghancha@gmail.com>
2020-12-23 18:28:59 +0900

    jonghan button1

--- MergeTest/app/src/main/java/com/android/mergetest/MainActivity.java
+++ MergeTest/app/src/main/java/com/android/mergetest/MainActivity.java
@@ -9,15 +9,26 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    Button button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        button1 = findViewById(R.id.btn1);
+        button1.setOnClickListener(mClickListener);
     }
 
+    View.OnClickListener mClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.btn1:
+                    Toast.makeText(MainActivity.this, "Button 1 Clicked", Toast.LENGTH_SHORT).show();
+                    break;
+            }
+        }
+    };
 
 }
